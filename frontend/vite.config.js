19d8a7a3d483f4d3fcf2b408fb13a74c52da4ce0
Anushakoppula-2131 <anushakoppula2131@gmail.com>
2025-06-14 14:39:08 +0530

    edited vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Replace "SmartToDoList" with your repo name (case-sensitive)
 export default defineConfig({
   plugins: [react()],
-  base: './' 
+  base: '/SmartToDoList/'
 })
